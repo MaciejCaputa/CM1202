c3994ca91395f8ead1cce07e8d32ceb53db3a597
Maciej Caputa <maciej.caputa.pl@gmail.com>
2016-03-11 14:59:18 +0000

    @Maciej Caputa

--- users.py
+++ users.py
@@ -2,48 +2,49 @@
 	@Maciej Caputa
 """
 
+
 class User:
-	"""docstring for ClassName"""
-	def __init__(self, username, forename, surname, hashedPassword):
-		self.__username = username
-		self.__forename = forename
-		self.__surname = surname
-		self.__hashedPassword = hashedPassword
+    """docstring for ClassName"""
+    def __init__(self, username, forename, surname, hashedPassword):
+        self.__username = username
+        self.__forename = forename
+        self.__surname = surname
+        self.__hashedPassword = hashedPassword
 
-	def getUsername(self):
-		return self.__username
+    def getUsername(self):
+        return self.__username
 
-	def getForename(self):
-		return self.__forename
+    def getForename(self):
+        return self.__forename
 
-	def getSurname(self):
-		return self.__surname
-
+    def getSurname(self):
+        return self.__surname
 
 
 class Administrator(User):
-	"""docstring for ClassName"""
-	pass
-		
+    """docstring for ClassName"""
+    pass
+
 
 class Lecturer(User):
-	"""docstring for ClassName"""
-	def __init__(self, username, forename, surname, hashedPassword, school, modulesTaught):
-		super(Lecturer, self).__init__(username, forename, surname, hashedPassword)
-		self.__school = school
-		self.__modulesTaught = modulesTaught
+    """docstring for ClassName"""
+    def __init__(self, username, forename, surname,
+                 hashedPassword, school, modulesTaught):
+        super(Lecturer, self).__init__(username, forename, surname,
+                                       hashedPassword)
+        self.__school = school
+        self.__modulesTaught = modulesTaught
 
 
 class Student(User):
-	"""docstring for ClassName"""
-	def __init__(self, username, forename, surname, hashedPassword, year, course):
-		super(Student, self).__init__(username, forename, surname, hashedPassword)
-		self.__year = year
-		self.__course = course
-		
-	def getYear(self):
-		return self.__year
+    """docstring for ClassName"""
+    def __init__(self, username, forename, surname, hashedPassword, year, course):
+        super(Student, self).__init__(username, forename, surname, hashedPassword)
+        self.__year = year
+        self.__course = course
 
-	def getCourse(self):
-		return self.__course
+    def getYear(self):
+        return self.__year
 
+    def getCourse(self):
+        return self.__course
