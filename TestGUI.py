59f969d9c48a917ea490f78578e3b46d1a505dd0
Sarah Howells <sarah@howellsnet.org>
2016-04-14 15:40:07 +0100

    Added test title and introduction text (row spacing needs to be sorted)

--- TestGUI.py
+++ TestGUI.py
@@ -22,7 +22,7 @@ class TestGUI(tk.Frame):
 
         # Need to make a canvas with a frame inside it in order to scroll: see
         # here: http://stackoverflow.com/questions/3085696/adding-a-scrollbar-to-a-group-of-widgets-in-tkinter/3092341#3092341
-        canvas = tk.Canvas(self, yscrollcommand=scrollbar.set, width=800, height=600)
+        canvas = tk.Canvas(self, yscrollcommand=scrollbar.set, width=1000, height=600)
         canvas.pack(side=TOP, expand=True)
         scrollbar.configure(command=canvas.yview)
         frame = tk.Frame(canvas)
@@ -39,10 +39,16 @@ class TestGUI(tk.Frame):
 
         # Keep track of the submit buttons so we can remove them once clicked
         self.submit_buttons = []
+        
+        testTitle = Label(frame, text=test.getTestName(), font=('MS', 12, 'bold'), wraplength=750)
+        testTitle.grid(row = 1, column = 1, columnspan = 7)
+        
+        introductionText = Label(frame, text=test.getIntroductionText(), font=('MS', 10, 'bold'), wraplength=750)
+        introductionText.grid(row = 2, column = 1, columnspan = 7)
 
         for idx, question in enumerate(test.getNextQuestion(), start=0): # usage of Test class generator (yielding)
             task = Label(frame, text = "Task " + (str(idx + 1) + ". " + question.getQuestionText()), font = ('MS', 10, 'bold'), justify=LEFT, wraplength=500)
-            task.grid(row = idx * 10 + 1, column = 1, columnspan = 7)
+            task.grid(row = idx * 10 + 6, column = 1, columnspan = 7)
 
             if question.isQuestionMultipleChoice():
                 options = question.getOptions()
@@ -54,22 +60,22 @@ class TestGUI(tk.Frame):
                 # Displaying answers
                 for i in range(4):
                     radio[i] = Radiobutton(frame, variable=self.vars[-1], value=i+1)
-                    radio[i].grid(row = idx * 10 + 2, column=i * 2 + 1, sticky=E)
+                    radio[i].grid(row = idx * 10 + 8, column=i * 2 + 1, sticky=E)
                     answer[i] = Label(frame, text = options[i], font = ('MS', 10, 'bold'))
-                    answer[i].grid(row = idx * 10 + 2, column=i * 2 + 2, sticky=W)
+                    answer[i].grid(row = idx * 10 + 8, column=i * 2 + 2, sticky=W)
 
             else:
                 entAns = Entry(frame)
-                entAns.grid(row = idx * 10 + 2, column = 1, columnspan=4)
+                entAns.grid(row = idx * 10 + 8, column = 1, columnspan=4)
                 self.vars.append(entAns)
 
 
             mark = Label(frame, text = '(' + str(question.getAvailableMarks()) + ' marks)', font = ('MS', 10, 'bold'))
-            mark.grid(row = idx * 10 + 6, column = 8, sticky=E)
+            mark.grid(row = idx * 10 + 8, column = 8, sticky=E)
 
             butSubmit = Button(frame, text = 'Submit', font = ('MS', 10,'bold'))
             butSubmit['command'] = self.submit_command(question, idx, test)
-            butSubmit.grid(row = idx * 10 + 7, column = 8, sticky=E)
+            butSubmit.grid(row = idx * 10 + 10, column = 8, sticky=E)
             self.submit_buttons.append(butSubmit)
 
             # If it is a multiple choice q then answer is 1,2,3 or 4, NOT the
@@ -88,7 +94,7 @@ class TestGUI(tk.Frame):
             
         testSubmit = Button(frame, text = 'Submit Test', font = ('MS', 10, 'bold'))
         testSubmit['command'] = self.submitTest()
-        testSubmit.grid(row = idx * 10 + 8, column = 4, sticky=E)
+        testSubmit.grid(row = idx * 10 + 11, column = 4, sticky=E)
         
         
 
@@ -104,9 +110,9 @@ class TestGUI(tk.Frame):
         print("Correct answer is " + question.getCorrectAnswer())
 
         if question.isAnswerCorrect(USER_ID, answer):
-            self.correctLabels[idx].grid(row=idx * 10 + 3, column=1, sticky=W)
+            self.correctLabels[idx].grid(row=idx * 10 + 12, column=1, sticky=W)
         else:
-            self.incorrectLabels[idx].grid(row=idx * 10 + 3, column=1, sticky=W)
+            self.incorrectLabels[idx].grid(row=idx * 10 + 12, column=1, sticky=W)
 
         self.submit_buttons[idx].grid_remove()
         test.addQuestionResult(USER_ID, question)
